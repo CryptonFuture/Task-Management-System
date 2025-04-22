85b37def6c192c49ae71df5c44e88e18f8bafd77
Hamza Rafique <160319542+hamzarafique345@users.noreply.github.com>
2025-04-23 00:34:11 +0500

    Upgrade

---
+++ src/app/Layouts/dashboardLayout.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import LeftSidebar from '../components/(Navbar)/leftsidebar'
+import Navbar from '../components/(Navbar)/navbar'
+
+export default function Dashboardlayout({children}: {children: React.ReactNode}) {
+  return (
+    <div>
+                <div className="flex min-h-screen">
+                  <LeftSidebar />
+                  <div className="flex-1 flex flex-col">
+                    <Navbar />
+                    <div className="flex-1 p-4">
+                      {children}
+                    </div>
+                  </div>
+                </div>
+
+    </div>
+  )
+}
---
+++ src/app/components/(Navbar)/navbar.tsx
@@ -0,0 +1,13 @@
+// components/TopNavbar.js
+export default function Navbar() {
+    return (
+      <nav className="bg-blue-600 text-white p-4">
+        <div className="flex justify-between items-center">
+          <div>
+                <input type="text"  />
+          </div>
+        </div>
+      </nav>
+    );
+  }
+  
