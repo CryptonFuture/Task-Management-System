fb35dd8a58a6da0aa469f032c70683b877517deb
Hamza Rafique <160319542+hamzarafique345@users.noreply.github.com>
2025-04-26 01:41:15 +0500

    chart and responsive work

--- src/app/components/(Dashboard)/Card.tsx
+++ src/app/components/(Dashboard)/Card.tsx
@@ -3,19 +3,30 @@ import Image from "next/image";
 export default function Card({ Link, title, value, difference }: any) {
   return (
     <div className="flex flex-col bg-white p-4 rounded-xl shadow-md hover:shadow-lg transition-shadow duration-300">
-      <div className="flex items-center justify-between mb-4">
-        <h2 className="text-sm text-gray-500">{title}</h2>
-        <h3 className="text-2xl font-semibold text-gray-800">{value}</h3>
+      <div className="flex items-center justify-between mb-3">
+        <h2 className="text-sm md:text-base text-gray-500">{title}</h2>
+        <h3 className="text-xl md:text-2xl font-semibold text-gray-800">
+          {value}
+        </h3>
       </div>
 
       <div className="flex items-center justify-between gap-4">
-        <div className="w-36 h-24 relative flex-shrink-0">
-          <Image src={Link} alt={title} fill className="object-contain" />
+        <div className="relative w-22 sm:h-30 sm:w-36 h-22 md:w-38 md:h-24">
+          <Image src={Link} alt={title} fill className="object-contain " />
         </div>
 
-        <p className="text-sm text-green-600">
-          {difference} more <br /> from last week
-        </p>
+        <div className=" relative right-1 sm:left-2 text-right flex">
+          <p className="text-[10px] sm:text-xs md:text-sm text-green-600">
+            <strong className=" text-[12px] sm:text-[14px] md:text-[16px] text-green-600">
+              {" "}
+              {difference}
+            </strong>{" "}
+            <span className="text-[#768396]">
+              {" "}
+              more <br /> from last week
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,10 @@
-import Card from "./components/(Dashboard)/Card";
-import DashboardLayout from "./Layouts/dashboardLayout";
+import React from 'react'
+import Dashboard from './pages/Dashboard'
 
 export default function Home() {
   return (
-<DashboardLayout>
-      <div className="flex flex-col min-h-screen bg-gray-100">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-5 mx-4 md:mx-10 py-10">
-          <Card
-            title="New Task"
-            value={10}
-            difference="+10"
-            Link="/BlueLine.png"
-          />
-          <Card
-            title="Task Completed"
-            value={8}
-            difference="+10"
-            Link="/BlueLine.png"
-          />
-          <Card
-            title="Project Done"
-            value={10}
-            difference="+8"
-            Link="/RedLine.png"
-          />
-          <Card
-            title="In Progress"
-            value={10}
-            difference="+6"
-            Link="/RedLine.png"
-          />
-        </div>     
-      </div>
-</DashboardLayout>
+    <div>
+      <Dashboard/>
+    </div>
   )
 }
